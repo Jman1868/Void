807d9701773cebd4518fb3f8994cb9d96c136afb
FrostDragonGames <43161576+Jman1868@users.noreply.github.com>
2023-04-03 11:42:24 -0700

    made css for textcard component

---
+++ components/TextCard.js
@@ -0,0 +1,18 @@
+import styles from '@/styles/index.module.css'
+
+
+
+export default function TextCard(props)
+{
+
+
+    return (
+
+        
+        <div className={styles.card}>
+            <p className={styles.voidText}>The void types back</p>
+            <div className={styles.textCard}>  </div>
+            
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,38 @@ import styles from '@/styles/index.module.css'
 import React from 'react'
 import Link from "next/link"
 import { FaRegQuestionCircle } from "react-icons/fa"
+import TextCard from '../../components/TextCard'
 
-export default function Home() {
+
+
+export async function GetStaticProps()
+{
+  const res = await fetch('https://anapioficeandfire.com/api/characters/583')
+  const data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+
+
+
+export default function Home({data}) {
  
-  const [test, setTest]= React.useState(true)
+
+  console.log(data)
+  const [submit, setSubmit]= React.useState(true)
  
-  function boho()
+  function HandelSubmit()
   {
     
-    setTest(test => !test)
-    console.log(test)
+    setSubmit(prev => !prev)
+      console.log(submit)
   }
+
 
 
   return (
@@ -23,12 +44,19 @@ export default function Home() {
       
       <Link href="/faq" className='link'><FaRegQuestionCircle className={styles.bigIcon} /></Link>
       
-      <div className={styles.card}>
+      {submit && <div className={styles.card}>
 
         <p className={styles.voidText}>Type into the void</p>
         <textarea width="400"></textarea>
-        <button className={ styles.greenButton} onClick={boho}>Submit</button>
+        <button className={styles.greenButton} onClick={HandelSubmit}>Submit</button>
       </div>
+      }
+
+      {!submit && <div className={styles.card}>
+
+        <TextCard  />
+        
+      </div>}
       
 
       
